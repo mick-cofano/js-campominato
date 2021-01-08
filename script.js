26d42af774b73681e03b05e7e0939a54355914bb
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-08 17:12:22 +0100

    Sviluppato sistema per generare i 16 numeri random

--- script.js
+++ script.js
@@ -13,3 +13,27 @@
     // SE il numero non è presente L'UTENTE CONTINUA A GIOCARE
       // L'UTENTE continua a giocare fino a quando non becca un numero di quei 16 random (perdente)
       // o fino a quando non arriva al tentativo n.84, di conseguenza vince (vincente)
+
+
+// funzione che genera i numeri randomici
+function genNumeri(n, min, max) {
+    var mine = [];
+
+    // ripeto 16 volte => genero 16 numeri
+    for (var i = 0; i < n; i++) {
+        // utilizzare il ciclo per verificare che i numeri siano diversi
+        // se è un numero gia uscito ne genera un'altro
+        do {
+            var numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+        } while (mine.includes(numeroRandom));
+
+        mine.push(numeroRandom); //questo comando inserisce il numero nell'array che contiene le "mine"
+    }
+    return mine;
+}
+
+
+// output in console.log per generazione numeri random
+
+var numeriComputer = genNumeri(16, 1, 100);
+console.log(numeriComputer);
